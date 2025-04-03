2e08fe25160e3ceabf7e1b2897f5d86262848c8f
emmeiwhite <emmeiwhite@gmail.com>
2025-04-03 15:50:29 +0530

    c++ 1.1 data-types (int, char, float, bool, double)

--- day-1.cpp
+++ day-1.cpp
@@ -2,6 +2,28 @@
 using namespace std;
 
 int main() {
-    cout << "Hello, Imran! Your C++ setup is working perfectly." << endl;
+    // Let's declare some variables to hold values of particular type because "every value has a type that determines its role and every value is nothing but a piece of information"
+
+    int num = 23;
+    cout << num << endl;
+
+    char ch = 'I';
+    cout << ch << endl;
+
+    bool isPreparing = true;
+    cout << isPreparing << endl;
+
+    float f = 1.23;
+    cout << f << endl;
+
+    double d = 3.2;
+    cout << d << endl;
+
+
+    cout << "size of d is :" << sizeof(d);
+
+
+
+
     return 0;
 }
